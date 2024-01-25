13a6963dfcb38c4ce81caa58b3be34248ff1661e
YagovkinDmytro <139924381+YagovkinDmytro@users.noreply.github.com>
2024-01-25 18:37:37 +0100

    timer

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -4,6 +4,8 @@ import 'flatpickr/dist/flatpickr.min.css';
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
 
+let timerCounter = null;
+
 const elements = {
   input: document.querySelector('input'),
   button: document.querySelector('button[data-start]'),
@@ -26,42 +28,49 @@ const options = {
     if (selectedDates[0] < options.defaultDate) {
       iziToast.error({
         message: 'Please choose a date in the future',
+        position: 'topRight',
       });
     } else {
-      let differenceTime = selectedDates[0] - options.defaultDate;
+      options.differenceTime = selectedDates[0] - options.defaultDate;
       elements.button.removeAttribute('disabled');
-      options.differanceObj = convertMs(differenceTime);
     }
   },
 };
 
 flatpickr(elements.input, options);
 
-let cuntDifferense = null;
-cuntDifferense = setInterval(() => {}, 1000);
-
 function setCounter() {
-  elements.days.textContent = addLeadingZero(options.differanceObj.days);
-  elements.hours.textContent = addLeadingZero(options.differanceObj.hours);
-  elements.minutes.textContent = addLeadingZero(options.differanceObj.minutes);
-  elements.seconds.textContent = addLeadingZero(options.differanceObj.seconds);
+  timerCounter = setInterval(() => {
+    let counterTime = (options.differenceTime = `${
+      options.differenceTime - 1000
+    }`);
+    console.log(counterTime);
+    if (counterTime < 1000) {
+      clearInterval(timerCounter);
+    }
+    options.differanceObj = convertMs(counterTime);
+    elements.days.textContent = addLeadingZero(options.differanceObj.days);
+    elements.hours.textContent = addLeadingZero(options.differanceObj.hours);
+    elements.minutes.textContent = addLeadingZero(
+      options.differanceObj.minutes
+    );
+    elements.seconds.textContent = addLeadingZero(
+      options.differanceObj.seconds
+    );
+  }, 1000);
+
   elements.button.setAttribute('disabled', '');
 }
 
 function convertMs(ms) {
-  // Number of milliseconds per unit of time
   const second = 1000;
   const minute = second * 60;
   const hour = minute * 60;
   const day = hour * 24;
 
-  // Remaining days
   const days = Math.floor(ms / day);
-  // Remaining hours
   const hours = Math.floor((ms % day) / hour);
-  // Remaining minutes
   const minutes = Math.floor(((ms % day) % hour) / minute);
-  // Remaining seconds
   const seconds = Math.floor((((ms % day) % hour) % minute) / second);
 
   return { days, hours, minutes, seconds };
